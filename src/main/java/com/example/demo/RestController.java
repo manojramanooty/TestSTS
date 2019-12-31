775ded439c1da2b1126a6af9d0e9e268eedfa217
Manoj Raman (RBEI/ETA24) <noa1kor@bosch.com>
2019-12-31 11:56:01 +0530

    update response

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RestController {  
     @RequestMapping("/hello")  
     public String hello(){  
-        return"Hello config works!....hi priya";  
+        return"Hello config works!....hello world";  
     }  
 }  
